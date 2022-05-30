8bb54bbd666b70d6d9ef0733c67858d743e302e6
Evan Godon <e.godon@outlook.com>
2022-05-30 16:01:42 -0400

    fix icons in lualine

--- docs/ui.md
+++ docs/ui.md
@@ -2,3 +2,4 @@
 
 
 
+- The midi icons in the nerd fonts are centered compared to others
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -30,7 +30,7 @@ local diagnostics = {
 local diff = {
 	"diff",
 	colored = false,
-	symbols = { added = "  ", modified = "  ", removed = "  " }, -- changes diff symbols
+	symbols = { added = "ﰂ ", modified = "ﭟ ", removed = "ﯰ" }, -- changes diff symbols
 	cond = hide_in_width,
 }
 
@@ -66,16 +66,16 @@ lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "〡" },
+		component_separators = { left = "〡", right = "〡" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
 	sections = {
 		lualine_a = { mode },
-		lualine_b = { branch },
+		lualine_b = { branch, diff },
 		lualine_c = {},
-		lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_x = { spaces, "encoding", filetype },
 	},
 	inactive_sections = {
 		lualine_a = {},
