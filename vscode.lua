d75a52a7a0b9d166cc1c311fe16a151e66e2c47b
Evan Godon <e.godon@outlook.com>
2022-06-02 23:22:54 -0400

    move vscode commands to lua file

--- vscode.lua
+++ vscode.lua
@@ -1,11 +1,15 @@
 require "user.settings"
 require "user.keymaps"
 
-local map = vim.api.nvim_set_keymap
+local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-local vscodeCmd = function(cmd)
+local function vscodeCmd(cmd)
 	return ":call VSCodeNotify('" .. cmd .. "')<CR>"
 end
 
-map("n", "<leader>ut", vscodeCmd "workbench.action.selectTheme", opts)
+keymap("n", "<leader>ut", vscodeCmd "workbench.action.selectTheme", opts)
+
+-- Navigation
+keymap("n", "[b", vscodeCmd "workbench.action.previousEditor", opts)
+keymap("n", "]b", vscodeCmd "workbench.action.nextEditor", opts)
