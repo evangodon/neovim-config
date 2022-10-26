72a90c208581dea5127f6cc9274587ef0220b7a5
Evan Godon <e.godon@outlook.com>
2022-10-26 15:57:12 -0400

    fix: barbar integrating with nvim-tree

--- lua/plugins/config/barbar.lua
+++ lua/plugins/config/barbar.lua
@@ -1,23 +1,30 @@
 local bufferline = require "bufferline"
 
 bufferline.setup({
-	animation = false,
-	icon_separator_active = "▓",
-	icon_separator_inactive = "▓",
-	letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
+  animation = false,
+  icon_separator_active = "▓",
+  icon_separator_inactive = "▓",
+  letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
 })
 
 local nvim_tree_events = require "nvim-tree.events"
 local bufferline_api = require "bufferline.api"
 local fn = require "user.functions"
 
-nvim_tree_events.on_tree_open(function()
-	bufferline_api.set_offset(NvimTreeWidth + 1, "File Tree")
-	vim.cmd [[wincmd p]]
+local function get_tree_size()
+  return require("nvim-tree.view").View.width
+end
+
+nvim_tree_events.subscribe("TreeOpen", function()
+  bufferline_api.set_offset(get_tree_size() + 1)
 end)
 
-nvim_tree_events.on_tree_close(function()
-	bufferline_api.set_offset(0)
+nvim_tree_events.subscribe("Resize", function()
+  bufferline_api.set_offset(get_tree_size() + 1)
+end)
+
+nvim_tree_events.subscribe("TreeClose", function()
+  bufferline_api.set_offset(0)
 end)
 
 local keymap = vim.keymap.set
@@ -29,21 +36,21 @@ keymap("n", "<leader>\\", CMD "BufferPick", opts)
 
 -- create key map to navigate buffers
 local keymapBufferlineGoToBuffer = function()
-	local keymaps = {}
+  local keymaps = {}
 
-	for i = 8, 1, -1 do
-		keymaps[tostring(i)] = {
-			string.format(CMD "BufferGoto %d", i),
-			"which_key_ignore",
-		}
-	end
+  for i = 8, 1, -1 do
+    keymaps[tostring(i)] = {
+      string.format(CMD "BufferGoto %d", i),
+      "which_key_ignore",
+    }
+  end
 
-	keymaps["B"] = {
-		CMD "BufferCloseAllButCurrent",
-		"Close all buffers but current",
-	}
+  keymaps["B"] = {
+    CMD "BufferCloseAllButCurrent",
+    "Close all buffers but current",
+  }
 
-	return keymaps
+  return keymaps
 end
 
 fn.leaderKeymaps(keymapBufferlineGoToBuffer())
