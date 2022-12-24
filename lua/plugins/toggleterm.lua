03ee6ff547ea290001e2c2d1f80facb7b3cd7197
Evan Godon <e.godon@outlook.com>
2022-12-24 00:05:25 -0500

    feat: add lazygit term command

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,71 +2,77 @@
 -- https://github.com/akinsho/toggleterm.nvim
 --
 local M = {
-	"akinsho/toggleterm.nvim",
+  "akinsho/toggleterm.nvim",
 }
 
 function M.config()
-	local toggleterm = require "toggleterm"
+  local toggleterm = require "toggleterm"
 
-	local palette = require "catppuccin.palettes.mocha"
-	local termBgColor = palette.base
+  local palette = require "catppuccin.palettes.mocha"
+  local termBgColor = palette.base
 
-	toggleterm.setup({
-		size = 20,
-		open_mapping = [[<c-\>]],
-		hide_numbers = true,
-		start_in_insert = true,
-		insert_mappings = true,
-		persist_size = true,
-		direction = "float",
-		close_on_exit = true,
-		shell = vim.o.shell,
-		float_opts = {
-			border = "curved",
-		},
-		highlights = {
-			NormalFloat = {
-				guibg = termBgColor,
-			},
-			FloatBorder = {
-				guibg = termBgColor,
-				guifg = palette.mauve,
-			},
-		},
-	})
+  toggleterm.setup({
+    size = 20,
+    open_mapping = [[<c-\>]],
+    hide_numbers = true,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    direction = "float",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+      border = "curved",
+    },
+    highlights = {
+      NormalFloat = {
+        guibg = termBgColor,
+      },
+      FloatBorder = {
+        guibg = termBgColor,
+        guifg = palette.mauve,
+      },
+    },
+  })
 
-	-- Set terminal keymaps
-	vim.api.nvim_create_autocmd({ "TermOpen" }, {
-		callback = function()
-			local opts = { noremap = true }
-			vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-		end,
-	})
+  -- Set terminal keymaps
+  vim.api.nvim_create_autocmd({ "TermOpen" }, {
+    callback = function()
+      local opts = { noremap = true }
+      vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    end,
+  })
 
-	-- Custom Terminals
-	local Terminal = require("toggleterm.terminal").Terminal
+  -- Custom Terminals
+  local Terminal = require("toggleterm.terminal").Terminal
 
-	local todo = Terminal:new({ cmd = "todo view", close_on_exit = true })
+  local todo = Terminal:new({ cmd = "todo view", close_on_exit = true })
 
-	function _TODO_TOGGLE()
-		todo:toggle()
-	end
+  function _TODO_TOGGLE()
+    todo:toggle()
+  end
 
-	local dash = Terminal:new({ cmd = "dash", hidden = true })
+  local dash = Terminal:new({ cmd = "dash", hidden = true })
 
-	function _DASH_TOGGLE()
-		dash:toggle()
-	end
+  function _DASH_TOGGLE()
+    dash:toggle()
+  end
 
-	local fn = require "user.functions"
-	fn.registerKeyMap({
-		D = {
-			function()
-				dash:toggle()
-			end,
-			"Open [D]ashboard",
-		},
-	})
+  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+  function _LG_TOGGLE()
+    lazygit:toggle()
+  end
+
+  local fn = require "user.functions"
+  fn.registerKeyMap({
+    D = {
+      function()
+        dash:toggle()
+      end,
+      "Open [D]ashboard",
+    },
+  })
 end
 
 return M
