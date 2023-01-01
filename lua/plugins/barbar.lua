b05115e5b721d27ebffc9c32cca2159cf5c656e5
Evan Godon <e.godon@outlook.com>
2023-01-01 13:34:44 -0500

    fix: barbar pin icon

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,51 +1,54 @@
 local M = {
-	"romgrk/barbar.nvim",
-	event = "VeryLazy",
+  "romgrk/barbar.nvim",
+  event = "VeryLazy",
 }
 
 function M.config()
-	local bufferline = require "bufferline"
+  local bufferline = require "bufferline"
 
-	bufferline.setup({
-		animation = false,
-		icon_separator_active = "▓",
-		icon_separator_inactive = "▓",
-		letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
-	})
+  bufferline.setup({
+    animation = false,
+    icon_separator_active = "▓",
+    icon_separator_inactive = "▓",
+    icon_pinned = "車",
+    letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
+  })
+end
 
-	local nvim_tree_events = require "nvim-tree.events"
-	local bufferline_api = require "bufferline.api"
-	local fn = require "user.functions"
+function M.init()
+  local nvim_tree_events = require "nvim-tree.events"
+  local bufferline_api = require "bufferline.api"
+  local fn = require "user.functions"
 
-	local function get_tree_size()
-		return require("nvim-tree.view").View.width
-	end
+  local function get_tree_size()
+    return require("nvim-tree.view").View.width
+  end
 
-	nvim_tree_events.subscribe("TreeOpen", function()
-		bufferline_api.set_offset(get_tree_size() + 1)
-	end)
+  nvim_tree_events.subscribe("TreeOpen", function()
+    bufferline_api.set_offset(get_tree_size() + 1)
+  end)
 
-	nvim_tree_events.subscribe("Resize", function()
-		bufferline_api.set_offset(get_tree_size() + 1)
-	end)
+  nvim_tree_events.subscribe("Resize", function()
+    bufferline_api.set_offset(get_tree_size() + 1)
+  end)
 
-	nvim_tree_events.subscribe("TreeClose", function()
-		bufferline_api.set_offset(0)
-	end)
+  nvim_tree_events.subscribe("TreeClose", function()
+    bufferline_api.set_offset(0)
+  end)
 
-	local keymap = vim.keymap.set
-	local opts = { noremap = true, silent = true }
+  local keymap = vim.keymap.set
+  local opts = { noremap = true, silent = true }
 
-	keymap("n", "[B", CMD "BufferMovePrev", opts)
-	keymap("n", "]B", CMD "BufferMoveNext", opts)
-	keymap("n", "<leader>\\", CMD "BufferPick", opts)
+  keymap("n", "[B", CMD "BufferMovePrev", opts)
+  keymap("n", "]B", CMD "BufferMoveNext", opts)
+  keymap("n", "<leader>\\", CMD "BufferPick", opts)
 
-	fn.registerKeyMap({
-		B = {
-			CMD "BufferCloseAllButCurrent",
-			"Close all buffers but current",
-		},
-	})
+  fn.registerKeyMap({
+    B = {
+      CMD "BufferCloseAllButCurrent",
+      "Close all buffers but current",
+    },
+  })
 end
 
 return M
