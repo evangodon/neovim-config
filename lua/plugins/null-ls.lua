95cc9ec058ca68ee0b97ef236dbcf2e0a8879635
Evan Godon <e.godon@outlook.com>
2023-01-05 10:51:57 -0500

    formatting

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -2,46 +2,46 @@
 -- https://github.com/jose-elias-alvarez/null-ls.nvim
 
 local M = {
-  "jose-elias-alvarez/null-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  event = "BufReadPre",
+	"jose-elias-alvarez/null-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = "BufReadPre",
 }
 
 function M.config()
-  local null_ls = require "null-ls"
+	local null_ls = require "null-ls"
 
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-  local formatting = null_ls.builtins.formatting
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+	local formatting = null_ls.builtins.formatting
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 
-  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-  null_ls.setup({
-    debug = false,
-    sources = {
-      -- Lua
-      formatting.stylua,
-      -- JS
-      formatting.prettier,
-      -- Go
-      formatting.gofmt,
-      formatting.golines,
-      formatting.goimports,
-    },
-    on_attach = function(client, bufnr)
-      -- Format on save
-      if client.supports_method "textDocument/formatting" then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ bufnr = bufnr })
-          end,
-        })
-      end
-    end,
-  })
+	null_ls.setup({
+		debug = false,
+		sources = {
+			-- Lua
+			formatting.stylua,
+			-- JS
+			formatting.prettier,
+			-- Go
+			formatting.gofmt,
+			formatting.golines,
+			formatting.goimports,
+		},
+		on_attach = function(client, bufnr)
+			-- Format on save
+			if client.supports_method "textDocument/formatting" then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = bufnr })
+					end,
+				})
+			end
+		end,
+	})
 end
 
 return M
--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -2,42 +2,42 @@
 -- https://github.com/petertriho/nvim-scrollbar
 --
 local M = {
-	"petertriho/nvim-scrollbar",
-	cmd = { "ZenMode" },
-	event = "BufReadPre",
+  "petertriho/nvim-scrollbar",
+  cmd = { "ZenMode" },
+  event = "BufReadPre",
 }
 
 function M.config()
-	local scrollbar = require "scrollbar"
+  local scrollbar = require "scrollbar"
 
-	-- Setup gitsigns integration
-	local git_icon = "│"
-	require("scrollbar.handlers.gitsigns").setup()
+  -- Setup gitsigns integration
+  local git_icon = "│"
+  require("scrollbar.handlers.gitsigns").setup()
 
-	scrollbar.setup({
-		handle = {},
-		handlers = {
-			gitsigns = false,
-		},
-		marks = {
-			Cursor = {
-				text = "•",
-			},
-			GitAdd = {
-				text = git_icon,
-			},
-			GitChange = {
-				text = git_icon,
-			},
-			GitDelete = {
-				text = "▁",
-			},
-		},
-		show_in_active_only = true,
-		excluded_filetypes = {
-			"NvimTree",
-		},
-	})
+    scrollbar.setup({
+    handle = {},
+    handlers = {
+      gitsigns = false,
+    },
+    marks = {
+      Cursor = {
+        text = "•",
+      },
+      GitAdd = {
+        text = git_icon,
+      },
+      GitChange = {
+        text = git_icon,
+      },
+      GitDelete = {
+        text = "▁",
+      },
+    },
+    show_in_active_only = true,
+    excluded_filetypes = {
+      "NvimTree",
+    },
+  })
 end
 
 return M
