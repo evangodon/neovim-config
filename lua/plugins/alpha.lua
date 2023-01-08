04ef9202181953b9826afa2fa1e37eb9ec96e438
Evan Godon <e.godon@outlook.com>
2023-01-08 15:38:38 -0500

    update alpha dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,47 +2,49 @@
 -- https://github.com/goolord/alpha-nvim
 
 local M = {
-	"goolord/alpha-nvim",
-	dependencies = { "kyazdani42/nvim-web-devicons" },
-	lazy = false,
+  "goolord/alpha-nvim",
+  dependencies = { "kyazdani42/nvim-web-devicons" },
+  lazy = false,
 }
 
 function M.config()
-	local alpha = require "alpha"
-	local fn = require "user.functions.utils"
+  local alpha = require "alpha"
+  local fn = require "user.functions.utils"
 
-	local dashboard = require "alpha.themes.dashboard"
+  local dashboard = require "alpha.themes.dashboard"
+  local header = "▒▒  NEOVIM  ▒▒"
 
-	dashboard.section.header.val = { "", "", "▒▒  NEOVIM  ▒▒" }
-	dashboard.section.buttons.val = {
-		dashboard.button("p", "  Find project", CMD "Telescope workspaces"),
-		dashboard.button("r", "  Recently used files", CMD "Telescope oldfiles "),
-		dashboard.button("s", "痢 Sync plugins", CMD "CreateSnapshotAndSyncPlugins"),
-		dashboard.button("c", "  Configuration", CMD "e ~/.config/nvim/init.lua "),
-		dashboard.button("t", "  Open Todo list", CMD ":edit ~/.config/nvim/todo.md"),
-		dashboard.button("q", "  Quit", CMD "qa"),
-	}
+  dashboard.section.header.val = { "", "", header, "" }
+  dashboard.section.header.opts = { position = "center" }
 
-	local function footer()
-		return ""
-	end
+  dashboard.section.buttons.val = {
+    dashboard.button("p", "  Find project", CMD "Telescope workspaces  initial_mode=normal"),
+    dashboard.button("r", "  Recently used files", CMD "Telescope oldfiles "),
+    dashboard.button("c", "  Neovim Config", CMD "edit ~/.config/nvim/"),
+    dashboard.button("t", "  Open Todo list", CMD ":edit ~/.config/nvim/todo.md"),
+    dashboard.button("q", "  Quit", CMD "qa"),
+  }
 
-	dashboard.section.footer.val = footer()
+  local function footer()
+    return vim.fn.getcwd()
+  end
 
-	dashboard.section.footer.opts.hl = "Type"
-	dashboard.section.header.opts.hl = "Include"
-	dashboard.section.buttons.opts.hl = "Keyword"
+  dashboard.section.footer.val = footer()
 
-	dashboard.opts.opts.noautocmd = true
+  dashboard.section.footer.opts.hl = "Include"
+  dashboard.section.header.opts.hl = "Include"
+  dashboard.section.buttons.opts.hl = "Keyword"
 
-	fn.register_key_map({
-		A = {
-			":Alpha<CR>",
-			"Open Alpha dashboard",
-		},
-	})
+  dashboard.opts.opts.noautocmd = true
 
-	alpha.setup(dashboard.opts)
+  fn.register_key_map({
+    A = {
+      ":Alpha<CR>",
+      "Open Alpha dashboard",
+    },
+  })
+
+  alpha.setup(dashboard.opts)
 end
 
 return M
