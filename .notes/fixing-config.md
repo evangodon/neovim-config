a389cf32cd59fcadc5deb35ffee6354c1c2ebf69
Evan Godon <e.godon@outlook.com>
2023-01-09 12:59:49 -0500

    use sumneko_lua lsp for formatting instead of stylua

---
+++ .notes/fixing-config.md
@@ -0,0 +1,4 @@
+# Plugin breaks
+
+If config is broken after an update, remove the changes from the lazy-lock.json file
+and run `Lazy Restore` to restore the state of the lockfile.
--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -1,15 +1,23 @@
 return {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim", "require" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand "$VIMRUNTIME/lua"] = true,
-					[vim.fn.stdpath "config" .. "/lua"] = true,
-				},
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = "tab",
+          tab_width = "2",
+          indent_width = "2",
+        },
+      },
+      diagnostics = {
+        globals = { "vim", "require" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+    },
+  },
 }
