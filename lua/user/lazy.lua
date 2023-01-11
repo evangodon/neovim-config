9589477386f957ce6818f306cf31c06d6886d896
Evan Godon <e.godon@outlook.com>
2023-01-11 10:43:55 -0500

    add border to lazy window

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,28 +1,31 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"git@github.com:folke/lazy.nvim.git",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "git@github.com:folke/lazy.nvim.git",
+    lazypath,
+  })
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 vim.g.mapleader = " "
 
 require("lazy").setup("plugins", {
-	defaults = { lazy = true },
-	checker = { enabled = false },
-	change_detection = {
-		enabled = false,
-	},
-	debug = false,
+  defaults = { lazy = true },
+  checker = { enabled = false },
+  change_detection = {
+    enabled = false,
+  },
+  debug = false,
+  ui = {
+    border = "single"
+  }
 })
 
 require("user.functions").register_key_map({ L = {
-	CMD "Lazy",
-	"Open Lazy manager",
+  CMD "Lazy",
+  "Open Lazy manager",
 } })
