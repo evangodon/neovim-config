fee981d44f2a06425b573a6b8839d5380dd79c91
Evan Godon <evan.godon@lightspeedhq.com>
2024-08-28 09:23:05 -0400

    update icon used for markdown list bullet

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,6 +4,16 @@ return {
   opts = {},
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("render-markdown").setup({})
+    require("render-markdown").setup({
+      bullet = {
+        icons = { "•", "○", "◆", "◇" },
+        -- Padding to add to the left of bullet point
+        left_pad = 0,
+        -- Padding to add to the right of bullet point
+        right_pad = 1,
+        -- Highlight for the bullet icon
+        highlight = "RenderMarkdownBullet",
+      },
+    })
   end,
 }
