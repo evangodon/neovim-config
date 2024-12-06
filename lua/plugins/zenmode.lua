1fea8a101e7cc70bdffd52c80ee71d993ef11112
Evan Godon <e.godon@outlook.com>
2024-12-06 17:31:04 -0500

    update Zen mode for increasing font-size

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -9,6 +9,8 @@ local M = {
 function M.config()
   local zenmode = require "zen-mode"
 
+  local kitty_font_group_id = vim.api.nvim_create_augroup("KittyFontReset", { clear = true })
+
   zenmode.setup({
     window = {
       backdrop = 1,
@@ -27,14 +29,26 @@ function M.config()
         ScrollbarHide
         Gitsigns toggle_signs false
 		]]
+      vim.fn.system "kitty @ set-font-size +2"
+
       vim.keymap.set("n", "q", CMD "close", { noremap = true, silent = true })
+      vim.api.nvim_create_autocmd("VimLeavePre", {
+        group = kitty_font_group_id,
+        callback = function()
+          vim.fn.system "kitty @ set-font-size 0"
+        end,
+        desc = "Reset Kitty font size when exiting Neovim",
+      })
     end,
     on_close = function()
       vim.cmd [[
         ScrollbarShow
         Gitsigns toggle_signs true
       ]]
+
+      vim.fn.system "kitty @ set-font-size 0"
       vim.keymap.set("n", "q", "<Nop>")
+      vim.api.nvim_clear_autocmds({ group = kitty_font_group_id })
     end,
   })
 end
